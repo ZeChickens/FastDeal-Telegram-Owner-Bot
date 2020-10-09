028bd37e26bede4a2df1764418a9c7f94cc78639
HYaroslav <gslaavko@gmail.com>
2020-10-09 19:12:58 +0300

    fixed small bug

--- bot.py
+++ bot.py
@@ -182,6 +182,4 @@ if __name__ == "__main__":
     # Start flask server
     app.run(host=WEBHOOK_LISTEN,
             port=WEBHOOK_PORT,
-            ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
-            debug=True)
-            debug=True)
+            ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV), debug=True)
